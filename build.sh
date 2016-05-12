d033e1e996820e389901d122a0d154db055f31d6
Rick Alm <rickalm@aol.com>
2016-05-12 18:37:47 +0000

    Working on SystemD launching docker

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-docker build -f Dockerfile.centos-dind-systemd:7 -t rickalm/centos-dind-systemd:7 .
+docker build -f Dockerfile.centos-dind-systemd:7 -t rickalm/centos7-dind-systemd:7 .
