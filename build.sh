d1a1c9711c1992921a75a2fbef4bf9f3569d822a
Rick Alm <rickalm@aol.com>
2016-05-27 14:29:13 +0000

    Change build to prefer pull from docker hub

--- build.sh
+++ build.sh
@@ -1,5 +1,17 @@
 #! /bin/bash
 
-docker build -f Dockerfile.centos7:base -t rickalm/centos7:base .
-docker build -f Dockerfile.centos7:dind-systemd -t rickalm/centos7:dind-systemd .
-docker build -f Dockerfile.centos7:systemd-dind -t rickalm/centos7:systemd-dind .
+# Since these are built by hub.docker.com, no need to build locally anymore
+# but as a backup if they cannot be pulled then create locally
+#
+
+docker pull rickalm/centos7:base ||
+  ( echo Building rickalm/centos7:base locally && 
+    docker build -f Dockerfile.centos7:base -t rickalm/centos7:base . )
+
+docker pull rickalm/centos7:dind-systemd || 
+  ( echo Building rickalm/centos7:dind-systemd locally && 
+    docker build -f Dockerfile.centos7:dind-systemd -t rickalm/centos7:dind-systemd . )
+
+docker pull rickalm/centos7:systemd-dind ||
+  ( echo Building rickalm/centos7:systemd-dind locally && 
+    docker build -f Dockerfile.centos7:systemd-dind -t rickalm/centos7:systemd-dind . )
