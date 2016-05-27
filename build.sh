ede88ae8a0c3f0ee78fa7ceefde3df4881292558
Rick Alm <rickalm@aol.com>
2016-05-27 19:37:18 +0000

    cleanup

--- build.sh
+++ build.sh
@@ -8,10 +8,10 @@ docker pull rickalm/centos7:base ||
   ( echo Building rickalm/centos7:base locally && 
     docker build -f Dockerfile.centos7:base -t rickalm/centos7:base . )
 
-docker pull rickalm/centos7:dind-systemd || 
-  ( echo Building rickalm/centos7:dind-systemd locally && 
-    docker build -f Dockerfile.centos7:dind-systemd -t rickalm/centos7:dind-systemd . )
+docker pull rickalm/centos7-dind:dind-systemd || 
+  ( echo Building rickalm/centos7-dind:dind-systemd locally && 
+    docker build -f Dockerfile.centos7-dind:dind-systemd -t rickalm/centos7-dind:dind-systemd . )
 
-docker pull rickalm/centos7:systemd-dind ||
-  ( echo Building rickalm/centos7:systemd-dind locally && 
-    docker build -f Dockerfile.centos7:systemd-dind -t rickalm/centos7:systemd-dind . )
+docker pull rickalm/centos7-dind:systemd-dind ||
+  ( echo Building rickalm/centos7-dind:systemd-dind locally && 
+    docker build -f Dockerfile.centos7-dind:systemd-dind -t rickalm/centos7-dind:systemd-dind . )
