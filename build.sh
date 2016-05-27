33b601deea3268a97553ccf9d41140ca8f158863
Rick Alm <rickalm@aol.com>
2016-05-27 13:19:32 +0000

    rename repos

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-docker build -f Dockerfile.centos7-base:7 -t rickalm/centos7-base:7 .
-docker build -f Dockerfile.centos7-dind-systemd:7 -t rickalm/centos7-dind-systemd:7 .
-docker build -f Dockerfile.centos7-systemd-dind:7 -t rickalm/centos7-systemd-dind:7 .
+docker build -f Dockerfile.centos7:base -t rickalm/centos7:base .
+docker build -f Dockerfile.centos7:dind-systemd -t rickalm/centos7:dind-systemd .
+docker build -f Dockerfile.centos7:systemd-dind -t rickalm/centos7:systemd-dind .
