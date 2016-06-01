eda75cd4789cf7c936deed5eb23bdf09f18c7257
Rick Alm <rickalm@aol.com>
2016-06-01 16:55:18 +0000

    push to build 006

--- build.sh
+++ build.sh
@@ -3,15 +3,16 @@
 # Since these are built by hub.docker.com, no need to build locally anymore
 # but as a backup if they cannot be pulled then create locally
 #
+[ -n "$1" ] && rev=-${1}
 
-docker pull rickalm/centos7:base ||
-  ( echo Building rickalm/centos7:base locally && 
-    docker build -f Dockerfile.centos7:base -t rickalm/centos7:base . )
+docker pull rickalm/centos7:base${rev} ||
+  ( echo Building rickalm/centos7:base${rev} locally && 
+    docker build -f Dockerfile.centos7:base -t rickalm/centos7:base${rev} . )
 
-docker pull rickalm/centos7-dind:dind-systemd || 
-  ( echo Building rickalm/centos7-dind:dind-systemd locally && 
-    docker build -f Dockerfile.centos7-dind:dind-systemd -t rickalm/centos7-dind:dind-systemd . )
+docker pull rickalm/centos7-dind:dind-systemd${rev} || 
+  ( echo Building rickalm/centos7-dind:dind-systemd${rev} locally && 
+    docker build -f Dockerfile.centos7-dind:dind-systemd${rev} -t rickalm/centos7-dind:dind-systemd${rev} . )
 
-docker pull rickalm/centos7-dind:systemd-dind ||
-  ( echo Building rickalm/centos7-dind:systemd-dind locally && 
-    docker build -f Dockerfile.centos7-dind:systemd-dind -t rickalm/centos7-dind:systemd-dind . )
+docker pull rickalm/centos7-dind:systemd-dind${rev} ||
+  ( echo Building rickalm/centos7-dind:systemd-dind${rev} locally && 
+    docker build -f Dockerfile.centos7-dind:systemd-dind${rev} -t rickalm/centos7-dind:systemd-dind${rev} . )
